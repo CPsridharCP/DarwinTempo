e51c7e8381385ff7724f37feac03f1a73e6aaba2
CPsridharCP <scp392@nyu.edu>
2017-04-10 02:25:58 +0530

    Add files via upload

--- slow_squart.py
+++ slow_squart.py
@@ -41,6 +41,36 @@ if __name__=="__main__":
     rospy.sleep(1)
 
     darwin.set_angles({"j_pan": 1})
+    
+    steps = 5.0
+
+    j_high_arm_r = 1.343676989
+    j_high_arm_l = 1.378893255
+
+    j_shoulder_r = 0.831647219
+    j_shoulder_l = 0.711766896
+    
+    j_low_arm_r = 1.499488737
+    j_low_arm_l = -1.508986304
+
+    
+    j_high_arm_r_path = path(0.0,j_high_arm_r,steps)
+    j_high_arm_l_path = path(0.0,j_high_arm_l,steps)
+    j_shoulder_r_path = path(0.0,j_shoulder_r,steps)
+    j_shoulder_l_path = path(0.0,j_shoulder_l,steps)
+    j_low_arm_r_path  = path(0.0,j_low_arm_r,steps)
+    j_low_arm_l_path  = path(0.0,j_low_arm_l,steps)
+
+    for i in range(0,int(steps)+1):
+    	darwin.set_angles({"j_high_arm_r": j_high_arm_r_path[i]})
+    	darwin.set_angles({"j_high_arm_l": j_high_arm_l_path[i]})
+    	darwin.set_angles({"j_shoulder_r": j_shoulder_r_path[i]})
+    	darwin.set_angles({"j_shoulder_l": j_shoulder_l_path[i]})
+    	darwin.set_angles({"j_low_arm_r": j_low_arm_r_path[i]})
+    	darwin.set_angles({"j_low_arm_l": j_low_arm_l_path[i]})
+    	rospy.sleep(1)
+
+
 
 
 
