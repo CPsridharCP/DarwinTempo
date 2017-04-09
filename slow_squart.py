8f5fa9a586df735c62eda63dd1ee144982ec44c6
CPsridharCP <scp392@nyu.edu>
2017-04-10 02:52:28 +0530

    Add files via upload

--- slow_squart.py
+++ slow_squart.py
@@ -43,15 +43,25 @@ if __name__=="__main__":
     darwin.set_angles({"j_pan": 1})
     
     steps = 5.0
-
+    
+    # enter all the desired joint angles here
     j_high_arm_r = 1.343676989
     j_high_arm_l = 1.378893255
 
-    j_shoulder_r = 0.831647219
+    j_shoulder_r = -0.831647219
     j_shoulder_l = 0.711766896
     
     j_low_arm_r = 1.499488737
     j_low_arm_l = -1.508986304
+
+    j_thigh2_r = -1.074252644
+    j_thigh2_l = 1.032503093
+
+    j_tibia_r = 2.208412228
+    j_tibia_l = -2.203297003
+
+    j_ankle1_r = 1.063030457
+    j_ankle1_l = -1.091408566
 
     
     j_high_arm_r_path = path(0.0,j_high_arm_r,steps)
@@ -60,6 +70,20 @@ if __name__=="__main__":
     j_shoulder_l_path = path(0.0,j_shoulder_l,steps)
     j_low_arm_r_path  = path(0.0,j_low_arm_r,steps)
     j_low_arm_l_path  = path(0.0,j_low_arm_l,steps)
+    j_thigh2_r_path   = path(0.0,j_thigh2_r,steps)
+    j_thigh2_l_path   = path(0.0,j_thigh2_r,steps)
+    j_tibia_r_path    = path(0.0,j_tibia_r,steps)
+    j_tibia_l_path    = path(0.0,j_tibia_l,steps)
+    j_ankle1_r_path   = path(0.0,j_ankle1_r,steps)
+    j_ankle1_l_path   = path(0.0,j_ankle1_r,steps)
+
+
+    darwin.set_angles({"j_ankle1_r": 0.1})
+    darwin.set_angles({"j_ankle1_l": -0.1})
+    darwin.set_angles({"j_thigh2_r": -0.20})
+    darwin.set_angles({"j_thigh2_l": 0.20})
+    darwin.set_angles({"j_tibia_r": 0.20})
+    darwin.set_angles({"j_tibia_l": -0.20})
 
     for i in range(0,int(steps)+1):
     	darwin.set_angles({"j_high_arm_r": j_high_arm_r_path[i]})
@@ -68,6 +92,12 @@ if __name__=="__main__":
     	darwin.set_angles({"j_shoulder_l": j_shoulder_l_path[i]})
     	darwin.set_angles({"j_low_arm_r": j_low_arm_r_path[i]})
     	darwin.set_angles({"j_low_arm_l": j_low_arm_l_path[i]})
+    	darwin.set_angles({"j_thigh2_r": j_thigh2_r_path[i]})
+    	darwin.set_angles({"j_thigh2_r": j_thigh2_l_path[i]})
+    	darwin.set_angles({"j_tibia_r": j_tibia_r_path[i]})
+    	darwin.set_angles({"j_tibia_l": j_tibia_l_path[i]})
+    	darwin.set_angles({"j_ankle1_r": j_ankle1_r_path[i]})
+    	darwin.set_angles({"j_ankle1_l": j_ankle1_l_path[i]})
     	rospy.sleep(1)
 
 
